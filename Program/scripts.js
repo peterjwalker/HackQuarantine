afbbd05a7e558f5db404e47282e7e61a430d0bbc
Peter Walker <60064444+peterjwalker@users.noreply.github.com>
2020-03-26 06:37:06 +0000

    Add files via upload

--- Program/scripts.js
+++ Program/scripts.js
@@ -1,17 +1,19 @@
+var isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "40%";
+    if (isMobile){
+        document.getElementById("mySidenav").style.width = "100%";
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "40%";
+    }
+
 }
   
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-}
 
-function Mobile(){
-    if (isMobileDevice() == true){
-        
-        document.getElementById("mySidenav").style.width = "100%";
-    }
-}
+//function Mobile(){
+//    alert("hey");
+//    }
